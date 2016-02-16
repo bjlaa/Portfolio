a946862fd0579e8b0a1bc3cef7dcd08c8425cea5
Benjamin <benjamin.j.arias@gmail.com>
2016-02-16 11:23:22 +0100

    feat: installed classnames react

--- components/work.js
+++ components/work.js
@@ -10,25 +10,25 @@ class Work extends React.Component {
 	render() {
 		return (
 			<div className="work">
-				<WorkItem data={data} />
+				<WorkList data={data} />
 			</div>
 		)
 
 	}
 }
 
-class WorkItem extends React.Component {
+class WorkList extends React.Component {
 	render() {
 		var work = this.props.data.work.map(function(e) {
 			return (
-				<div className="work-item" key={e.id}>
+				<div key={e.id}>
 					<img src={e.imageURL} alt=""/>
 					<h4>{e.title}</h4>
 				</div>
 			)
 		});
 		return (
-			<div className="work-item">
+			<div className="work-list">
 				{work}
 			</div>
 		)
