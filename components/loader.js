166c012c471f0c5f89610b1d9e8f5fb8a1235edf
Benjamin <benjamin.j.arias@gmail.com>
2016-04-07 13:22:45 +0200

    feat: Modified position of hourglass

--- components/loader.js
+++ components/loader.js
@@ -9,7 +9,9 @@ import React from 'react';
 class Loader extends React.Component {
 	render() {
 		return(
-			<div className="loader">Loading
+			<div className="loader">Loading...
+				<br/>
+				<br/>
 				<i className="hourglass fa fa-hourglass"></i>
 			</div>
 
