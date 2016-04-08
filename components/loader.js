2ca268067eac394f5ed8ce2e86868d5c7b7c0525
Benjamin <benjamin.j.arias@gmail.com>
2016-04-08 20:11:26 +0200

    fix: Issue with z-index

--- components/loader.js
+++ components/loader.js
@@ -9,10 +9,13 @@ import React from 'react';
 class Loader extends React.Component {
 	render() {
 		return(
-			<div className="loader">Loading...
-				<br/>
-				<br/>
-				<i className="hourglass fa fa-hourglass"></i>
+			<div className="loader">
+				<div className="circle">
+					Loading...
+					<br/>
+					<br/>
+					<i className="hourglass fa fa-hourglass"></i>
+				</div>
 			</div>
 
 		)
