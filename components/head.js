a17ca344cca012b96a87b414f4adc973f2da1dfa
Benjamin <benjamin.j.arias@gmail.com>
2016-04-26 18:35:15 +0200

    feat: changed title

--- components/head.js
+++ components/head.js
@@ -8,7 +8,7 @@ class Head extends React.Component {
 	render() {
 		return (
 			<div className="head">
-					<div className='title-name'>B. Arias</div>
+					<div className='title-name'>Benjamin Arias</div>
 					<div className='title-sub'>Front-End Developer</div>
 			</div>
 		)
